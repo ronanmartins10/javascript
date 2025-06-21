f10516b088270a9cc5cd48e71d13f0877998ca8d
Ronan Martins <ronanmartinsoliveira10@gmail.com>
2025-06-21 15:24:51 -0300

    estudando

--- exercicios/aula12ex/ex014/script.js
+++ exercicios/aula12ex/ex014/script.js
@@ -1,10 +1,16 @@
-function carregar() {
+function atualizarHora() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
+    var minutos = data.getMinutes()
+    var segundos = data.getSeconds()
 
+    hora = hora < 10 ? '0' + hora : hora;
+    minutos = minutos < 10 ? '0' + minutos : minutos;
+    segundos = segundos < 10 ? '0' + segundos : segundos;
+
+    msg.innerHTML = `Agora são ${hora}:${minutos}:${segundos}.`
     if (hora >=0 && hora < 12) {
         // bom dia 
     img.src = 'manha.jpg'
@@ -18,4 +24,9 @@ function carregar() {
     img.src = 'noite.jpg'
     document.body.style.background = '#0b0b42';
     }
+}
+
+  function iniciarRelogio() {
+    atualizarHora() 
+    setInterval(atualizarHora, 1000) 
 }
